3619b6837886e2e4bd7f403570df5704b426c139
Chris Wang <chriswang@Mac.localdomain>
2026-06-13 02:12:54 +0800

    Point V2 frontend to latest Apps Script deployment

--- v2/config.js
+++ v2/config.js
@@ -1,5 +1,5 @@
 window.ILIONS_V2_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbwvhU-VQGTKKrSXjjLWBEX0_5TZW-WnF62adG5tDxPHD0YCdskgogZnpBw7Y84w_TRu/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbxaAlGb0cLpKDgsD8Ywg30Km1chNVhZ_KpCyeV714wp6VmD4z4wuDtC-sAkamr5ec8J/exec",
   liffId: "2010373009-hrgqoUsM",
   dashboardToken: "ilions-v2-test-dashboard-2026"
 };
