d1775f0db21ccb1fecfedf89a7809d0f11d7fb6d
Chris Wang <chriswang@Mac.localdomain>
2026-06-13 22:54:31 +0800

    Point frontend to latest LINE auth deployment

--- v2/config.js
+++ v2/config.js
@@ -1,5 +1,5 @@
 window.ILIONS_V2_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbwX_UKWq8ydAToYEZWlUlrB9FyCNI2Inlhoj0MwKeGZdQi3isEYkvsnD4Q7LcCUNdd6/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbyoBrNwrRYv0XTLS92CeXNAj6QCU-r7PwmFl1-g-I1_lWAtcEyISyfmdAy0T5x4Txeu/exec",
   liffId: "2010373009-hrgqoUsM",
   dashboardToken: "ilions-v2-test-dashboard-2026"
 };
