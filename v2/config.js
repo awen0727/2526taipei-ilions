26e1955cb133eede0e33b14f7f97061cfe8a4cc6
Chris Wang <chriswang@Mac.localdomain>
2026-06-14 17:39:37 +0800

    Point frontend to guest report deployment

--- v2/config.js
+++ v2/config.js
@@ -1,5 +1,5 @@
 window.ILIONS_V2_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbwFEfNgux_HHdcyAho8KFSrd1b-0Lht-TXMGq58B-XNF1o0jnOTKHgsztvyV7oQOL9W/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbxeXPCxS77_NdvWFKm69Wgx7kLAiH0Kxt4YeXpd_hT2WA-nWfwgEICMx_6bmOtWb4RA/exec",
   liffId: "2010373009-hrgqoUsM",
   dashboardToken: "ilions-v2-test-dashboard-2026"
 };
