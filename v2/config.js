52b225eb60b177b6fbb0a3915cd11a069288ddc6
Chris Wang <chriswang@Mac.localdomain>
2026-06-14 21:49:10 +0800

    Point frontend to role ordered reports deployment

--- v2/config.js
+++ v2/config.js
@@ -1,5 +1,5 @@
 window.ILIONS_V2_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbzaIU7KxiFp86KJiKlBsLzSl0_LeXzgVga2yJ0LjkXCapOnaisgDT47BT1ac2UCpnk_/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbwbTZ6pujUu9IBnLXVYIa2akuL1DZWWKVtL4si0zH5qGWaUreAAmsx9KQB-1u9VXeAa/exec",
   liffId: "2010373009-hrgqoUsM",
   dashboardToken: "ilions-v2-test-dashboard-2026"
 };
