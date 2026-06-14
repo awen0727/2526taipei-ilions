2b1287e3f443006f060364a67fed268515e44b31
Chris Wang <chriswang@Mac.localdomain>
2026-06-14 21:01:10 +0800

    Point frontend to admin management deployment

--- v2/config.js
+++ v2/config.js
@@ -1,5 +1,5 @@
 window.ILIONS_V2_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbxeXPCxS77_NdvWFKm69Wgx7kLAiH0Kxt4YeXpd_hT2WA-nWfwgEICMx_6bmOtWb4RA/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbwE8Hz3XxFHy1dcSstM3hsq2XdwteJbMatStZb2wMGZTvSpLjuzXzvYZhrpK9S00GNO/exec",
   liffId: "2010373009-hrgqoUsM",
   dashboardToken: "ilions-v2-test-dashboard-2026"
 };
