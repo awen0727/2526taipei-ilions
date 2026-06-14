75ca2a957e13b8427773453921156c4a202385b4
Chris Wang <chriswang@Mac.localdomain>
2026-06-14 21:27:26 +0800

    Point frontend to manual check-in deployment

--- v2/config.js
+++ v2/config.js
@@ -1,5 +1,5 @@
 window.ILIONS_V2_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbwE8Hz3XxFHy1dcSstM3hsq2XdwteJbMatStZb2wMGZTvSpLjuzXzvYZhrpK9S00GNO/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbzaIU7KxiFp86KJiKlBsLzSl0_LeXzgVga2yJ0LjkXCapOnaisgDT47BT1ac2UCpnk_/exec",
   liffId: "2010373009-hrgqoUsM",
   dashboardToken: "ilions-v2-test-dashboard-2026"
 };
